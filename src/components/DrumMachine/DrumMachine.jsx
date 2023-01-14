1c95ceddac742df656156b135e4460b767f0d615
natalia-ponomarenko <virus.zabutiya@gmail.com>
2023-01-14 18:27:08 +0200

    add some styles to buttons, background, display

--- src/components/DrumMachine/DrumMachine.jsx
+++ src/components/DrumMachine/DrumMachine.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { Display } from "../Display/Display";
 import { DrumPadGroup } from "../DrumPadGroup/DrumPadGroup";
-import "./DrumMachine.scss"
+import "./DrumMachine.scss";
 
 export function DrumMachine() {
-  const [soundName, setSoundName] = useState('');
+  const [soundName, setSoundName] = useState("");
 
   return (
-    <div id="drum-machine">
-      <DrumPadGroup setSoundName={setSoundName} />
-      <Display soundName={soundName} />
+    <div class="background-container">
+      <img src="../../images/galaxy.png" alt="" />
+      <div class="stars"></div>
+      <div class="twinkling"></div>
+      <div id="drum-machine">
+        <DrumPadGroup setSoundName={setSoundName} />
+        <Display soundName={soundName} />
+      </div>
     </div>
-    );
+  );
 }
