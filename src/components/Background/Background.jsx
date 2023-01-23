a401a97c7bb344b874a2b51fbf0034491259e5eb
natalia-ponomarenko <virus.zabutiya@gmail.com>
2023-01-23 15:52:58 +0200

    removed image

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -3,11 +3,6 @@ import './Background.scss';
 export function Background({ children }) {
   return (
     <div className="background background__container">
-      <img
-        // src="../../images/galaxy.png"
-        alt="galaxy"
-        className="background__image"
-      />
       <div className="background__stars"></div>
       <div className="background__twinkling"></div>
       {children}
